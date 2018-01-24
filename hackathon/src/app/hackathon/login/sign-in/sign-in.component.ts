21537916dfa05cda077104c2199570482e0d91d5
itsvinayonline <vinayonline@yahoo.com>
2018-01-24 15:09:46 +0530

    sign-in and sign-up page code

--- hackathon/src/app/hackathon/login/sign-in/sign-in.component.ts
+++ hackathon/src/app/hackathon/login/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginStateService } from '../../../store/services/login-state.service';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  error = '';
+
+  constructor(private router: Router, private loginStateService: LoginStateService) { }
 
   ngOnInit() {
   }
 
+  signIn() {
+
+        console.log('Login in process');
+        console.log(this.model.username, this.model.password);
+      this.loginStateService.login(this.model.username, this.model.password)
+          .subscribe(result => {
+              if (result === true) {
+                  this.router.navigate(['/']);
+              } else {
+                  this.error = 'Username or password is incorrect';
+              }
+          });
+
+    }
 }
--- hackathon/src/app/hackathon/login/sign-up/sign-up.component.ts
+++ hackathon/src/app/hackathon/login/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  model: any = {};
+  public info: any = {};
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
+  signUp() {
+
+    this.info = {
+      username: this.model.username,
+      password: this.model.password,
+      confirmPassword: this.model.confirmpassword,
+      email: this.model.email,
+      primarySkillSet: this.model.skillSet
+     };
+    console.log('signup in process');
+    console.log(this.info);
+
+    this.info.primarySkillSet = this.info.primarySkillSet.split(/[ ,]+/).filter(Boolean);
+    console.log(this.info);
+         this.http.post('', this.info)
+         .subscribe(
+               () => {},
+               err => console.log(err)
+           );
+    }
 }
