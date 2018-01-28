70af6276f82d92b6472fe1b11fbd9a365818162b
MadhaviCN <madhavi1ss010@gmail.com>
2018-01-28 17:47:27 +0530

    Request to hacker page changes

--- hackathon/src/app/shared/search-display-request-to-hacker/search-display-request-to-hacker.component.ts
+++ hackathon/src/app/shared/search-display-request-to-hacker/search-display-request-to-hacker.component.ts
@@ -25,7 +25,7 @@ export class SearchDisplayRequestToHackerComponent implements OnInit {
   constructor(private hackerStateService: HackerStateService) { }
 
   ngOnInit() {
-    console.log(this.HackerDetails.hackerList);
+    //console.log(this.HackerDetails.hackerList);
     // if (this.HackerDetails && this.HackerDetails.ideas) {
     //   this.hackIdea = this.HackerDetails.data.users[0].username;
     //   this.imgSrc = this.HackerDetails.data.users[0].image;
@@ -38,7 +38,6 @@ export class SearchDisplayRequestToHackerComponent implements OnInit {
   }
 
   public requestHacker() {
-    console.log(this.HackerDetails.data.users);
     console.log(this.userId);
     this.hackerStateService.requestHacker({data: {userId: this.userId} }); // Once API ready, need to integrate
 
@@ -51,10 +50,10 @@ export class SearchDisplayRequestToHackerComponent implements OnInit {
     if(this.hackIdea !== "Choose one"){
       this.flag = true;
       for (let i = 0; i < this.HackerDetails.hackerList.data.users.length; i++) {
-        if (idea === this.HackerDetails.data.hackerList.users[i].username) {
+        if (idea === this.HackerDetails.hackerList.data.users[i].username) {
           this.imgSrc = this.HackerDetails.hackerList.data.users[i].image;
           this.email = this.HackerDetails.hackerList.data.users[i].email;
-          this.skill = this.HackerDetails.hackerList.data.users[i].skillsets;
+          this.skill = this.HackerDetails.hackerList.data.users[i].profile.skillset[0];
           this.userId = this.HackerDetails.hackerList.data.users[i]._id;
         }else{
           this.email = "";
